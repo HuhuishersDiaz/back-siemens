6257c4ff0d5c61efa21e27782f429f52fa43973e
ArmandoDiazDiaz <armando.diaz.diaz.2409@gmail.com>
2021-10-27 10:45:35 -0500

    fix email

--- functions/index.js
+++ functions/index.js
@@ -23,27 +23,28 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(cors);
 
 const routers_countries = require('./routes/countries');
-const routers_users = require('./routes/users');
-const routers_normative = require('./routes/products/normative');
-const routers_area = require('./routes/products/aplication_area');
-const routers_type = require('./routes/products/type');
-const routers_products = require('./routes/products/products');
-const training = require('./routes/training/training');
-const localization = require('./routes/branches/branches');
+const { use } = require('./routes/countries');
+// const routers_users = require('./routes/users');
+// const routers_normative = require('./routes/products/normative');
+// const routers_area = require('./routes/products/aplication_area');
+// const routers_type = require('./routes/products/type');
+// const routers_products = require('./routes/products/products');
+// const training = require('./routes/training/training');
+// const localization = require('./routes/branches/branches');
 
 app.use('/api/countries', routers_countries);
-app.use('/api/users', routers_users);
-app.use('/api/products/normatives', routers_normative);
-app.use('/api/products/area', routers_area);
-app.use('/api/products/type', routers_type);
-app.use('/api/products/', routers_products);
-app.use('/api/training/', training);
-app.use('/api/localization/', localization);
+// app.use('/api/users', routers_users);
+// app.use('/api/products/normatives', routers_normative);
+// app.use('/api/products/area', routers_area);
+// app.use('/api/products/type', routers_type);
+// app.use('/api/products/', routers_products);
+// app.use('/api/training/', training);
+// app.use('/api/localization/', localization);
 
-// /product_aplication_area
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/views/index.html'));
-});
+// // /product_aplication_area
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname + '/views/index.html'));
+// });
 app.post('/sendEmail', (req, res) => {
 	let { nombre, email, tel, fecha, hora } = req.body;
 	console.log('Entramos al metodo ');
@@ -74,6 +75,152 @@ app.post('/sendEmail', (req, res) => {
 			res.json('Email sent: ' + info.response);
 		}
 	});
+});
+
+// exports.app = functions.https.onRequest(app);
+
+// const functions = require('firebase-functions');
+// const express = require('express');
+// const cors = require('cors')({ origin: true });
+// const admin = require('firebase-admin');
+
+// var nodemailer = require('nodemailer');
+
+// serviceAccount = require('./config/siemens-app-e35e2-firebase-adminsdk-94hte-5a6db71b1e.json');
+// admin.initializeApp({
+// 	credential: admin.credential.cert(serviceAccount),
+// 	databaseURL: 'https://siemens-app-e35e2-default-rtdb.firebaseio.com',
+// 	storageBucket: 'gs://siemens-app-e35e2.appspot.com',
+// });
+
+// const db = admin.firestore();
+// const app = express();
+
+const middleware = (req, res, next) => {
+	console.log(req.headers);
+	if (!req.headers.authorization) {
+		return res.status(403).json({ error: 'No credentials sent!' });
+	}
+	next();
+
+	// let authHeader = req.get('authorization');
+
+	// console.log(authHeader);
+	// // idToken comes from the client app
+	// admin
+	// 	.auth()
+	// 	.verifyIdToken(idToken)
+	// 	.then((decodedToken) => {
+	// 		const uid = decodedToken.uid;
+	// 		// ...
+	// 	})
+	// 	.catch((error) => {
+	// 		// Handle error
+	// 	});
+
+	// 	console.log('req');
+	// 	console.log('res');
+	// 	console.log('next');
+	// 	next();
+	// };
+};
+
+// app.use(express.json());
+
+// app.use(cors);
+
+// app.get('/sendEmail', (req, res) => {
+// 	console.log('Entramos al metodo ');
+// 	console.log(req.query);
+
+// 	var transporter = nodemailer.createTransport({
+// 		service: 'gmail',
+// 		auth: {
+// 			user: 'noreply@grupomedicogaleno.com',
+// 			pass: 'Galeno2021',
+// 		},
+// 	});
+
+// 	var mailOptions = {
+// 		from: 'noreply@grupomedicogaleno.com',
+// 		to: 'contacto@grupomedicogaleno.com,armando.diaz.diaz.2409@gmail.com',
+// 		subject: 'Nueva cita agendada ',
+// 		html: `Nombre:	${req.query.nombre} <br>
+// 		Correo:	${req.query.email}<br>
+// 		Telefono:${req.query.tel}<br>
+// 		Fecha:	${req.query.fecha}<br>
+// 		Hora:	${req.query.hora}`,
+// 	};
+
+// 	transporter.sendMail(mailOptions, function (error, info) {
+// 		console.log(error, info);
+// 		if (error) {
+// 			res.json(false);
+// 		} else {
+// 			res.json('Email sent: ' + info.response);
+// 		}
+// 	});
+// });
+
+app.post('/', middleware, async (req, res) => {
+	// app.post('/', async (req, res) => {
+	const db = admin.firestore().collection('fcmTokens');
+	const { title, description, url, role, user } = req.body;
+	var tokens = [];
+	if (user) {
+		console.log(user);
+		let token = await db
+			.doc(`${user}`)
+			.get()
+			.then((c) => c.data().token);
+		tokens.push(token);
+	} else if (role === 'all') {
+		tokens = await db
+			.where('token', '!=', 'null')
+			.get()
+			.then((c) => c.docs.map((doc) => doc.data().token));
+	} else {
+		tokens = await db
+			.where('role', '=', role)
+			.get()
+			.then((c) => c.docs.map((doc) => doc.data().token));
+	}
+
+	let notification = {
+		title: title,
+		description: description,
+		url: url,
+		role: role,
+		user: user,
+		createAt: new Date().toString(),
+	};
+
+	admin.firestore().collection('notifications').add(notification);
+	const message = {
+		data: {
+			url: url,
+			role: role,
+			createAt: new Date().toString(),
+		},
+		notification: {
+			body: description,
+			title: title,
+		},
+		tokens: tokens.filter((c) => c != null),
+	};
+
+	admin
+		.messaging()
+		.sendMulticast(message)
+		.then(() => {
+			res.json({ sending: 'true' }).statusCode = 201;
+		})
+		.catch((err) => {
+			res.json({ sending: 'false', err }).statusCode = 502;
+		});
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
 });
 
 exports.app = functions.https.onRequest(app);
